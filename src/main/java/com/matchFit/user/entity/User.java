b8ecb78aa37fd5d34f37ed179a671621eb0462a8
kimrma370 <kimrma370@gmail.com>
2025-07-21 15:22:20 +0900

    feat: 마이페이지 조회 서비스 로직 구현

--- src/main/java/com/matchFit/user/entity/User.java
+++ src/main/java/com/matchFit/user/entity/User.java
@@ -10,7 +10,9 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
+@Getter
 @Entity
 public class User extends BaseEntity {
 	@Id
--- src/main/java/com/matchFit/user/service/MyPageService.java
+++ src/main/java/com/matchFit/user/service/MyPageService.java
@@ -1,5 +1,30 @@
 package com.matchFit.user.service;
 
+import org.springframework.stereotype.Service;
+
+import com.matchFit.user.dto.response.MyPageResponse;
+import com.matchFit.user.entity.User;
+import com.matchFit.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class MyPageService {
+	
+	private final UserRepository userRepository;
+	
+    public MyPageResponse getMyPage(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        return MyPageResponse.builder()
+                .email(user.getEmail())
+                .nickName(user.getUsername())
+                .town(user.getTown())
+                .age(user.getAge())
+                .sports(user.getSports())
+                .build();
+    }
 
 }
