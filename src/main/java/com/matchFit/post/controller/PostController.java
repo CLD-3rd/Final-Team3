88c2ed76a667b68d9552977de4003578e873c3bd
alex052525 <alex62626@naver.com>
2025-07-21 17:24:12 +0900

    feat: 모집글 달력 조회 API 컨트롤러 구현

--- src/main/java/com/matchFit/post/controller/PostController.java
+++ src/main/java/com/matchFit/post/controller/PostController.java
@@ -1,11 +1,15 @@
 package com.matchFit.post.controller;
 
+import java.time.YearMonth;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matchFit.post.dto.response.GetPostsCalender;
 import com.matchFit.post.dto.response.GetPostsList;
 import com.matchFit.post.entity.Sports;
 import com.matchFit.post.service.PostService;
@@ -21,12 +25,20 @@ public class PostController {
 	private final PostService postService;
 	
 	@GetMapping("/list")
-    public ResponseEntity<GetPostsList> listPosts(
+    public ResponseEntity<GetPostsList> getPostsList(
 		@RequestParam(required = false) Sports sports,
         @RequestParam(required = false) Gender gender,
         @RequestParam(required = false, defaultValue = "false") boolean nearest
     ) {
-        GetPostsList postList = postService.findByFilters(sports, gender, nearest);
-        return ResponseEntity.ok(postList);
+        GetPostsList postsList = postService.findByFilters(sports, gender, nearest);
+        return ResponseEntity.ok(postsList);
     }
+	
+	@GetMapping("/calender")
+	public ResponseEntity<GetPostsCalender> getPostsCalender(
+			@RequestParam("month") @DateTimeFormat(pattern = "yyyy-MM") YearMonth month) {
+	    
+		GetPostsCalender postsCalender = postService.findByMonth(month);
+		return ResponseEntity.ok(postsCalender);
+	}
 }
