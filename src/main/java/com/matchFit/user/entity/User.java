6c014f5b7a836e5e3eb0aa6a96d69e3964b494f2
kimrma370 <kimrma370@gmail.com>
2025-07-21 16:51:12 +0900

    feat: 마이페이지 수정 서비스 로직 구현

--- src/main/java/com/matchFit/user/entity/User.java
+++ src/main/java/com/matchFit/user/entity/User.java
@@ -11,8 +11,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 public class User extends BaseEntity {
 	@Id
--- src/main/java/com/matchFit/user/service/MyPageService.java
+++ src/main/java/com/matchFit/user/service/MyPageService.java
@@ -1,7 +1,9 @@
 package com.matchFit.user.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.matchFit.user.dto.request.EditMyPageRequest;
 import com.matchFit.user.dto.response.MyPageResponse;
 import com.matchFit.user.entity.User;
 import com.matchFit.user.repository.UserRepository;
@@ -24,6 +26,26 @@ public class MyPageService {
                 .town(user.getTown())
                 .age(user.getAge())
                 .sports(user.getSports())
+                .build();
+    }
+    
+    @Transactional
+    public MyPageResponse editMyPage(String email, EditMyPageRequest req) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        user.setUsername(req.getNickName());
+        user.setAge(req.getAge());
+        user.setSports(req.getSports());
+
+        return MyPageResponse.builder()
+                .email(user.getEmail())
+                .nickName(user.getUsername())
+                .town(user.getTown())
+                .age(user.getAge())
+                .sports(user.getSports())
+                .recruitCount(user.getRecruitCount())
+                .joinCount(user.getJoinCount())
                 .build();
     }
 
