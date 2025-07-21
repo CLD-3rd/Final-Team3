418416abf53f825e8d9f6d829f50fe3a40f2d509
alex052525 <alex62626@naver.com>
2025-07-21 10:46:59 +0900

    add initial project

---
+++ src/test/java/com/matchFit/MatchFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
