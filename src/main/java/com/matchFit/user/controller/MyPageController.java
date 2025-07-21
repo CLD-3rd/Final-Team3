a8e6e2a7eaf65903643e08999086d8301d19b27d
kimrma370 <kimrma370@gmail.com>
2025-07-21 16:04:53 +0900

    feat: MyPageController 임시 추가

--- src/main/java/com/matchFit/user/controller/MyPageController.java
+++ src/main/java/com/matchFit/user/controller/MyPageController.java
@@ -1,5 +1,31 @@
 package com.matchFit.user.controller;
 
-public class MyPageController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.matchFit.user.dto.response.MyPageResponse;
+import com.matchFit.user.service.MyPageService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user/mypage")
+public class MyPageController {
+	
+    private final MyPageService myPageService;
+
+    @GetMapping
+    public ResponseEntity<MyPageResponse> getMyPage(@RequestHeader("accessToken") String token) {
+        String email = extractEmailFromToken(token);
+        return ResponseEntity.ok(myPageService.getMyPage(email));
+    }
+
+	private String extractEmailFromToken(String token) {
+		return token.replace("Bearer ", "");
+	}
+    
 }
