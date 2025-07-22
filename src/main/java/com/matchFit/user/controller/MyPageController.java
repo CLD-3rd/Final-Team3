d9c8aad083288cf438bc1c21392232e314bc4264
kimrma370 <kimrma370@gmail.com>
2025-07-22 13:01:06 +0900

    feat: 마이페이지 컨트롤러 jwt 기반으로 수정

--- src/main/java/com/matchFit/user/controller/MyPageController.java
+++ src/main/java/com/matchFit/user/controller/MyPageController.java
@@ -1,15 +1,16 @@
 package com.matchFit.user.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matchFit.user.dto.request.EditMyPageRequest;
 import com.matchFit.user.dto.response.MyPageResponse;
+import com.matchFit.user.jwt.JwtProvider;
 import com.matchFit.user.service.MyPageService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,19 +21,21 @@ import lombok.RequiredArgsConstructor;
 public class MyPageController {
 
     private final MyPageService myPageService;
+    private final JwtProvider jwtProvider;
 
     @GetMapping
-    public ResponseEntity<MyPageResponse> getMyPage(@AuthenticationPrincipal org.springframework.security.core.userdetails.User principal) {
-        String email = principal.getUsername(); // UserDetails의 username 필드에 이메일 저장됨
+    public ResponseEntity<MyPageResponse> getMyPage(@RequestHeader("Authorization") String authHeader) {
+        String email = jwtProvider.getEmailFromToken(authHeader.substring(7));
         return ResponseEntity.ok(myPageService.getMyPage(email));
     }
 
     @PutMapping
     public ResponseEntity<MyPageResponse> editMyPage(
-            @AuthenticationPrincipal org.springframework.security.core.userdetails.User principal,
+            @RequestHeader("Authorization") String authHeader,
             @RequestBody EditMyPageRequest request
     ) {
-        String email = principal.getUsername();
+        String email = jwtProvider.getEmailFromToken(authHeader.substring(7));
         return ResponseEntity.ok(myPageService.editMyPage(email, request));
     }
+    
 }
