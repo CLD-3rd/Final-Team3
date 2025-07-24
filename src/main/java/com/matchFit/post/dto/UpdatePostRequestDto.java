7f14e0080fa485911600ca442d8be475d690c644
sinascode <sina@dankook.ac.kr>
2025-07-24 13:02:16 +0900

    fix: 현재 시점 이전 모집글 수정 방지

---
+++ src/main/java/com/matchFit/post/dto/UpdatePostRequestDto.java
@@ -0,0 +1,27 @@
+package com.matchFit.post.dto;
+
+import java.time.LocalDateTime;
+
+import com.matchFit.post.entity.Sports;
+import com.matchFit.post.entity.Status;
+import com.matchFit.post.entity.Town;
+import com.matchFit.user.entity.Gender;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UpdatePostRequestDto {
+    private String title;
+    private String description;
+    private String location;
+    private LocalDateTime date;
+    private Integer maxPeople;
+    private Gender gender;
+    private Status status;
+    private Integer cost;
+    private String imageUrl;
+    private Sports sports;
+    private Town town;
+}
