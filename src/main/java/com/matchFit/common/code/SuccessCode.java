10894f812317b8f33175f7d723a63b1218dc7886
김정모 <116088682+alex052525@users.noreply.github.com>
2025-08-20 21:02:37 +0900

    fix: success code 중복 해결

--- src/main/java/com/matchFit/common/code/SuccessCode.java
+++ src/main/java/com/matchFit/common/code/SuccessCode.java
@@ -18,7 +18,7 @@ public enum SuccessCode {
 	POST_UPDATED("POST207", "모집글이 성공적으로 수정되었습니다."),
 	POST_GET_MY_APPLIED_POSTS("POST208", "내가 신청한 모집글 리스트를 성공적으로 조회했습니다."),
 	POST_DELETED("POST209", "모집글이 성공적으로 삭제되었습니다."),
-	POST_CANCEL_APPLY("POST209", "모집글 신청이 성공적으로 취소되었습니다."),
+	POST_CANCEL_APPLY("POST210", "모집글 신청이 성공적으로 취소되었습니다."),
   
 	// USER
   USER_CREATED("USER200", "회원가입이 성공적으로 완료되었습니다."),
